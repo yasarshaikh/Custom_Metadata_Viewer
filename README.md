09dbf60db5161eb88ae405f391e281897b8e4c28
Yasar Shaikh <yasar_shaikh@epam.com>
2022-08-17 19:48:56 +0530

    Base Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Custom Metadata Viewer
+This application will help users to navigate through the custom metadata types and it's records from single place.
+
---
+++ force-app/main/default/lwc/metadataViewer/metadataViewer.js
@@ -0,0 +1,87 @@
+import { LightningElement } from 'lwc';
+import fetchAvailableCustomMetadata from '@salesforce/apex/CustomMetadataService.fetchAvailableCustomMetadata';
+import getDetailsOf from '@salesforce/apex/CustomMetadataService.getDetailsOf';
+import getCustomMetadataRecords from '@salesforce/apex/CustomMetadataService.getCustomMetadataRecords';
+
+export default class MetadataViewer extends LightningElement {
+    metadataOptions = [];
+    showSpinner = true;
+    selectedCustomMetadata = '';
+    selectedButton = '';
+    metadataDetailWrapper = {};
+
+    detailData = [];
+    detailColumns = [
+        { label: 'Field Label', fieldName: 'fieldLabel' },
+        { label: 'Field Name', fieldName: 'fieldName' },        
+        { label: 'Field Type', fieldName: 'fieldType' }
+    ];
+
+    data = [];
+    dataColumns = [];
+
+    get options() {
+        return this.metadataOptions;
+    }
+
+    get showDetails() {
+        return this.selectedButton === 'details';
+    }
+    
+    get showRecords() {
+        return this.selectedButton === 'records';
+    }
+
+    connectedCallback() {
+        fetchAvailableCustomMetadata()
+            .then(result => {
+                console.log(result);
+                //iterate over the result and prepare option array with label and value
+                this.metadataOptions = result.map((item) => { return { label: item, value: item }});
+                this.showSpinner = false;
+            })
+            .catch(error => {
+                this.showSpinner = false;
+                console.error(error);
+            });
+    }
+
+    handleMetadataChange(event) {
+        this.selectedCustomMetadata = event.detail.value;
+    }
+
+    onShowDetails(event) {
+        this.selectedButton = 'details';
+        this.showSpinner = true;
+
+        getDetailsOf({ metadataApiName: this.selectedCustomMetadata })
+        .then(result =>{
+            console.log(result);
+            this.metadataDetailWrapper = result;
+            this.detailData = result.fields;
+            this.showSpinner = false;
+        })
+        .catch(error => {
+            this.showSpinner = false;
+            console.error(error);
+        });
+        
+    }
+
+    onShowRecords(event) {
+        this.selectedButton = 'records';
+        this.showSpinner = true;
+
+        getCustomMetadataRecords({ metadataApiName: this.selectedCustomMetadata })
+        .then(result => {
+            console.log(result);
+            this.data = result;
+            this.dataColumns = Object.keys(result[0]).map((item) => { return { label: item, fieldName: item }});
+            this.showSpinner = false;
+        })
+        .catch(error =>{
+            this.showSpinner = false;
+            console.error(error);
+        });
+    }
+}
